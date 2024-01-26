d50ab78cdeba35b2787c9ab6156486d7dea0a299
2heal1 <TwoHeal@163.com>
2024-01-26 11:44:18 +0800

    chore: comment dynamic import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import DDD from 'react-dom'
 import react from 'react'
 import remoteBtn from 'app1'
 
-import('./dynamic-import');
+// import('./dynamic-import');
 
 console.log('shared: ',DDD,react)
 console.log('remote: ',remoteBtn)
